be86a9bde6971074530f174ee7f30c8f6601aab1
Susanne Bülow <susanneb.hpi@gmail.com>
2017-01-28 18:33:05 +0100

    routing with geocoding and autocomplete on focusleft

--- app/src/main/java/com/bue/susanne/safeway/MainActivity.java
+++ app/src/main/java/com/bue/susanne/safeway/MainActivity.java
@@ -1,20 +1,28 @@
 package com.bue.susanne.safeway;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.ContentObserver;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Address;
 import android.location.Geocoder;
-import android.location.Location;
+
+import com.here.android.mpa.search.AutoSuggest;
+import  com.here.android.mpa.search.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.here.android.mpa.common.GeoCoordinate;
@@ -22,12 +30,17 @@ import com.here.android.mpa.common.OnEngineInitListener;
 import com.here.android.mpa.mapping.Map;
 import com.here.android.mpa.mapping.MapFragment;
 import com.here.android.mpa.mapping.MapMarker;
+import com.here.android.mpa.search.ErrorCode;
+import com.here.android.mpa.search.GeocodeRequest;
+import com.here.android.mpa.search.ResultListener;
+import com.here.android.mpa.search.TextAutoSuggestionRequest;
 
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.Manifest;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -45,19 +58,22 @@ public class MainActivity extends AppCompatActivity {
 
     // map embedded in the map fragment
     private Map map = null;
+    private Activity activity;
 
     // map fragment embedded in this activity
     private MapFragment mapFragment = null;
 
-    private SensorManager mgr;
     private LocationManager locationManager;
-    private Location currentLocation;
+    private android.location.Location currentLocation;
 
     private SensorEventListener listener;
     private LocationListener locationListener;
 
-    private TextView accelerationText;
-    private TextView locationText;
+    private boolean useCurrentLocation = false;
+
+    private String currentLocationString;
+
+    private SafeRouting routing;
 
     /**
      * permissions request code
@@ -68,36 +84,61 @@ public class MainActivity extends AppCompatActivity {
      * Permissions that need to be explicitly requested from end user.
      */
     private static final String[] REQUIRED_SDK_PERMISSIONS = new String[] {
-            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET };
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.INTERNET };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        activity = this;
         super.onCreate(savedInstanceState);
         checkPermissions();
-        initializeAcceleration();
+        initializeRouting();
     }
 
     public void onDestroy(){
         super.onDestroy();
-        mgr.unregisterListener(listener);
+    }
+
+    private void initializeRouting(){
+        final EditText startLocation = (EditText) findViewById(R.id.editStart);
+        final EditText endLocation = (EditText) findViewById(R.id.editEnd);
+
+        Button routeButton = (Button) findViewById(R.id.routingButton);
+
+        routeButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(v);
+                String start = startLocation.getText().toString();
+                if (start.equals("Your Location")){
+                    if (currentLocationString != null){
+                        start = currentLocationString;
+                        useCurrentLocation = true;
+                    }else{
+                        start = "Andreasstr. 10, Berlin";
+                        useCurrentLocation = false;
+                    }
+                }
+                String end = endLocation.getText().toString();
+
+                System.out.println(start + " - " + end);
+                geoCodeLocation(start, end);
+            }
+        });
     }
 
     private void initializeGPS(){
-        locationText = (TextView) findViewById(R.id.textCoordinates);
 
         locationManager = (LocationManager)
                 getSystemService(Context.LOCATION_SERVICE);
 
-
-
-
-
         locationListener = new LocationListener(){
             @Override
-            public void onLocationChanged(Location loc) {
+            public void onLocationChanged(android.location.Location loc) {
                 currentLocation = loc;
-                updateLocation(loc);
+                if (loc != null){
+                    updateLocation(loc);
+                }
             }
 
             @Override
@@ -115,14 +156,63 @@ public class MainActivity extends AppCompatActivity {
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0, locationListener);
         System.out.println(currentLocation);
+        if (currentLocation != null) {
+            updateLocation(currentLocation);
+        }
+    }
 
-        updateLocation(currentLocation);
+
+
+    private void geoCodeLocation(String start, String end){
+
+
+        final GeoCoordinate searchLocation = new GeoCoordinate(52.511390, 13.400027);
+        GeocodeRequest request = new GeocodeRequest(start).setSearchArea(searchLocation, 5000);
+
+        Location returnLocation;
+
+        class GeocodeListener implements ResultListener<List<Location>> {
+
+            public String endString;
+            Location start;
+            Location end;
+
+            @Override
+            public void onCompleted(List<Location> data, ErrorCode error) {
+
+                if (error != ErrorCode.NONE) {
+                    System.err.println(error);
+                } else {
+                    if (data.size() > 0){
+                        System.out.println(data.get(0).getCoordinate().getLatitude() + "," + data.get(0).getCoordinate().getLongitude());
+                    }
+                    System.out.println(endString);
+                    if (start == null){
+                        if (currentLocation != null && useCurrentLocation){
+                            start = new Location(new GeoCoordinate(currentLocation.getLatitude(), currentLocation.getLongitude()));
+                        }else{
+                            start = data.get(0);
+                        }
+                        System.out.println("Now geocode second location");
+                        GeocodeRequest request = new GeocodeRequest(endString).setSearchArea(searchLocation,5000);
+                        request.execute(this);
+                    }else{
+                        end = data.get(0);
+                      routing.calculateRoute(start.getCoordinate(), end.getCoordinate());
+                    }
+                }
+            }
+        }
+        GeocodeListener listener = new GeocodeListener();
+        listener.endString = end;
+        request.execute(listener);
+
+
 
     }
 
-    private void updateLocation(Location loc){
+    private void updateLocation(android.location.Location loc){
         /*------- To get city name from coordinates -------- */
-        String cityName = null;
         Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
         List<Address> addresses;
         try {
@@ -130,51 +220,65 @@ public class MainActivity extends AppCompatActivity {
                     loc.getLongitude(), 1);
             if (addresses.size() > 0) {
                 System.out.println(addresses.get(0).getLocality());
-                cityName = addresses.get(0).getLocality();
+                currentLocationString = addresses.get(0).toString();
             }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
 
-        locationText.setText(loc.getLatitude() + "," + loc.getLongitude() + " - " + cityName);
+       // locationText.setText(loc.getLatitude() + "," + loc.getLongitude() + " - " + cityName);
     }
 
-    private void initializeAcceleration(){
-        // mit dem SensorManager auf die Sensoren zugreifen
-        mgr = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    private void initializeAutoComplete(){
 
-        accelerationText = (TextView) findViewById(R.id.textAcceleration);
-
-        //create listener
-        listener = new SensorEventListener() {
+        class AutoSuggester implements View.OnFocusChangeListener {
 
             @Override
-            public void onSensorChanged(SensorEvent event) {
-                if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-                    float x,y,z;
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    System.out.println("Focus left " + v.getId());
+                    final MultiAutoCompleteTextView autoCompleteTextEdit = (MultiAutoCompleteTextView) v;
+                    String incompleteLocation = autoCompleteTextEdit.getText().toString();
+                    System.out.println(incompleteLocation);
+                    TextAutoSuggestionRequest request = null;
+                    request = new TextAutoSuggestionRequest(incompleteLocation).setSearchCenter(map.getCenter());
+                    class AutoSuggestionQueryListener implements ResultListener<List<AutoSuggest>> {
 
-                    x = event.values[0];
-                    y = event.values[1];
-                    z = event.values[2];
+                        @Override
+                        public void onCompleted(List<AutoSuggest> data, ErrorCode error) {
+                            if (error != ErrorCode.NONE) {
+                                System.err.println(error);
+                            } else {
+                                final String[] suggestions = new String[data.size()];
 
-                    // update textviews
-                    accelerationText.setText(x + "," + y + "," + z);
+
+                                for (int i = 0; i < data.size() && i < 5; i++) {
+                                    suggestions[i] = data.get(i).getTitle();
+                                    System.out.println(suggestions[i]);
+                                }
+                                ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity,
+                                        android.R.layout.simple_dropdown_item_1line, suggestions);
+                                autoCompleteTextEdit.setAdapter(adapter);
+
+                                autoCompleteTextEdit.showDropDown();
+                            }
+                        }
+                    }
+                    ;
+
+                    request.execute(new AutoSuggestionQueryListener());
                 }
-
+                ;
             }
+        }
 
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-                // unused
-            }
-        };
+        final MultiAutoCompleteTextView startLocation = (MultiAutoCompleteTextView) findViewById(R.id.editStart);
+        final EditText endLocation = (EditText) findViewById(R.id.editEnd);
 
+            startLocation.setOnFocusChangeListener(new AutoSuggester());
+        endLocation.setOnFocusChangeListener(new AutoSuggester());
 
-        // Den SensorListener bekannt machen.
-        mgr.registerListener(listener, mgr
-                        .getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     private void initialize() {
@@ -192,16 +296,20 @@ public class MainActivity extends AppCompatActivity {
                 if (error == OnEngineInitListener.Error.NONE) {
                     // retrieve a reference of the map from the map fragment
                     map = mapFragment.getMap();
+                    routing = new SafeRouting(map);
+                    initializeAutoComplete();
+
                     // Set the map center to the Vancouver region (no animation)
-                    map.setCenter(new GeoCoordinate(currentLocation.getLatitude(), currentLocation.getLongitude(), 0.0),
-                            Map.Animation.NONE);
+                    if (currentLocation != null){
+                        map.setCenter(new GeoCoordinate(currentLocation.getLatitude(), currentLocation.getLongitude(), 0.0),
+                                Map.Animation.NONE);
+                    }else{
+                        map.setCenter(new GeoCoordinate(52.511390, 13.400027, 0.0), Map.Animation.NONE);
+                    }
                     // Set the zoom level to the average between min and max
                     map.setZoomLevel(
                             (map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
 
-                    MapMarker marker = new MapMarker();
-                    marker.setCoordinate(new GeoCoordinate(currentLocation.getLatitude(), currentLocation.getLongitude()));
-                    map.addMapObject(marker);
                 } else {
                     System.out.println("ERROR: Cannot initialize Map Fragment");
                 }
